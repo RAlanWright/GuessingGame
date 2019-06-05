0c5bb7cb52f2ff3f08c97f0267bf4bf899bc27f3
RAlanWright <31636206+RAlanWright@users.noreply.github.com>
2019-06-04 19:01:37 -0500

    Added functionality including a hint to make it easier to finish the program

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -10,6 +10,33 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
+            string secretWord = "potato";
+            string guess = "";
+            string hint = "The relates to gardening and food.  The thing grows underground as a sprout, " +
+                        "and is usually brown when harvested.  If you do not cook/eat it soon enough, then it will " +
+                        "grow sprouts.  What could it be?";
+
+            Console.WriteLine("This is a guessing game.  You need to guess the correct word to stop the program");
+            Console.WriteLine("If you are having trouble, then enter 'hint'.");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            while (guess != secretWord)
+            {
+                if (guess == "hint")
+                {
+                    Console.WriteLine(hint);
+                    Console.WriteLine();
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine().ToLower();
+                } else
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine().ToLower();
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
