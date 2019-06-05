58128d6a18767fdbb553a3f643de7b7fbba33837
RAlanWright <31636206+RAlanWright@users.noreply.github.com>
2019-06-05 06:22:15 -0500

    Added beginning hint, win condition, and lose condition

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -12,6 +12,9 @@ namespace GuessingGame
         {
             string secretWord = "potato";
             string guess = "";
+            int guessCount = 0;
+            int guessLimit = 3;
+            bool outOfGuesses = false;
             string hint = "The relates to gardening and food.  The thing grows underground as a sprout, " +
                         "and is usually brown when harvested.  If you do not cook/eat it soon enough, then it will " +
                         "grow sprouts.  What could it be?";
@@ -19,21 +22,39 @@ namespace GuessingGame
             Console.WriteLine("This is a guessing game.  You need to guess the correct word to stop the program");
             Console.WriteLine("If you are having trouble, then enter 'hint'.");
             Console.WriteLine();
+            Console.WriteLine("The word is a type of food that contains 6 characters, and is eaten often in the United States.");
             Console.WriteLine();
 
-            while (guess != secretWord)
+            while (guess != secretWord && !outOfGuesses)
             {
-                if (guess == "hint")
+                if (guessCount < guessLimit)
                 {
-                    Console.WriteLine(hint);
-                    Console.WriteLine();
-                    Console.Write("Enter guess: ");
-                    guess = Console.ReadLine().ToLower();
+                    if (guess == "hint")
+                    {
+                        Console.WriteLine(hint);
+                        Console.WriteLine();
+                        Console.Write("Enter guess: ");
+                        guess = Console.ReadLine().ToLower();
+                    }
+                    else
+                    {
+                        Console.Write("Enter guess: ");
+                        guess = Console.ReadLine().ToLower();
+                        guessCount++;
+                    }
                 } else
                 {
-                    Console.Write("Enter guess: ");
-                    guess = Console.ReadLine().ToLower();
-                }
+                    outOfGuesses = true;
+                }              
+            }
+
+            if (outOfGuesses == false)
+            {
+                Console.WriteLine("You win!");
+            }
+            else
+            {
+                Console.WriteLine("Try again next time!");
             }
 
             Console.ReadLine();
